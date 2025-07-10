4b435bb66c11956939e2e245c2b27d037cccf50e
Chicha342 <86342851+Chicha342@users.noreply.github.com>
2025-07-10 15:22:39 +0300

    Initial Commit

---
+++ MacJson/MacJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacJsonApp.swift
+//  MacJson
+//
+//  Created by Никита on 10.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MacJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacJsonTests/MacJsonTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacJsonTests.swift
+//  MacJsonTests
+//
+//  Created by Никита on 10.07.2025.
+//
+
+import Testing
+@testable import MacJson
+
+struct MacJsonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
